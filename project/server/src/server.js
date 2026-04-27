830700cd9b9f5290ea6852cea1d8c33fb5f6c277
Chetan Lakade <1718chez@gmail.com>
2026-04-27 12:27:52 +0530

    Modify CORS settings for specific origin
    Updated CORS configuration to allow specific origin and credentials.

--- project/server/src/server.js
+++ project/server/src/server.js
@@ -18,9 +18,16 @@ import errorHandler from "./middleware/errorMiddleware.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://circu-trade-c7a1.vercel.app/",
+  credentials: true
+}));
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
