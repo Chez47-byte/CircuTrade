26dcc2ae9f690cd7f8d1903af8a01bb0860b0bd7
Chetan Lakade <1718chez@gmail.com>
2026-04-29 16:13:18 +0530

    Log API URL and Razorpay key in axios.js

--- project/client/src/api/axios.js
+++ project/client/src/api/axios.js
@@ -1,6 +1,9 @@
 
 import axios from "axios";
 
+console.log("BASE_URL:", import.meta.env.VITE_API_URL);
+console.log("RZP_KEY:", import.meta.env.VITE_RAZORPAY_KEY_ID);
+
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
